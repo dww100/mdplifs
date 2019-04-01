715586b6f89c52551da3f9c1978f546e295924d1
David W Wright <dave.william.wright@gmail.com>
2019-04-01 16:00:51 +0100

    First attempt at a CLI

---
+++ cli.py
@@ -0,0 +1,18 @@
+import docopt
+
+usage = """
+mdplifs
+
+Usage:
+  mdplifs (<topology> <trajectory>) [<ligand_selection>]
+  
+Arguments:
+  topology            System topology (currently only AMBER prmtop supported)
+  trajectory          Molecular dynamics trajectory
+  ligand_selection    Selection text for the ligand (default='resname LIG')
+"""
+
+
+def main(argv=None):
+
+    args = docopt(usage, argv=argv, version='0.0.1')
