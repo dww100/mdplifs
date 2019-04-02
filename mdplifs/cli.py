7dabf3e5607d831faa24fc2dcd61045701e2d40a
David W Wright <dave.william.wright@gmail.com>
2019-04-02 17:12:13 +0100

    Add frame selection to CLI, with basic validation and logging.

--- mdplifs/cli.py
+++ mdplifs/cli.py
@@ -1,19 +1,79 @@
+import sys
+import logging
 from docopt import docopt
 import mdtraj
 from . import featurization
 
+logger = logging.getLogger(__name__)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
 
 usage = """
 mdplifs
 
 Usage:
-  mdplifs (<topology> <trajectory>) [<ligand_selection>]
+  mdplifs (<topology> <trajectory>) [<ligand_selection>] [--first=<first> --last=<last> --stride=<stride>]
   
 Arguments:
   topology            System topology (currently only AMBER prmtop supported)
   trajectory          Molecular dynamics trajectory
   ligand_selection    Selection text for the ligand (default='resname LIG')
+  
+Options:
+  first               First frame to use in analysis
+  last                Last frame to use in analysis
+  stride              Step between frames to be used in analysis
 """
+
+
+def get_frame_range(traj, first, last, stride):
+
+    n_frames = traj.n_frames
+
+    if stride is None:
+        stride = 1
+    else:
+        stride = int(stride)
+
+    if last is None:
+        last = -1
+    else:
+        last = int(last)
+
+        if last < -1:
+            logger.critical('Last frame must be > -1 (selects final frame of '
+                            'trajectory)')
+            sys.exit(1)
+        elif last > n_frames:
+            logging.warning('Last frame selected after end of trajectory, '
+                            'using last frame.')
+            last = -1
+
+    if first is None:
+        first = 1
+    else:
+        first = int(first)
+        if first < -1 or (first > last and last != -1):
+            logger.critical('First frame must be > -1 and occur before the last'
+                            ' frame selected.')
+            sys.exit(1)
+
+    if stride > last - first and last != -1:
+        logger.warning('Selected frame stride greater than difference '
+                       'between first and last frame')
+
+    return slice(first, last, stride)
 
 
 def main(argv=None):
@@ -23,11 +83,18 @@ def main(argv=None):
     topology_filename = args['<topology>']
     trajectory_filename = args['<trajectory>']
     ligand_selection = args['<ligand_selection>']
+    first = args['--first']
+    last = args['--last']
+    stride = args['--stride']
 
     traj = mdtraj.load(trajectory_filename, top=topology_filename)
 
+    frames = get_frame_range(traj, first, last, stride)
+
     if ligand_selection is None:
-        featurization.Fingerprinter(traj, top_path=topology_filename)
+        featurization.Fingerprinter(traj, frames=frames,
+                                    top_path=topology_filename)
     else:
-        featurization.Fingerprinter(traj, top_path=topology_filename,
+        featurization.Fingerprinter(traj, frames=frames,
+                                    top_path=topology_filename,
                                     ligand_selection=ligand_selection)
